a22efb6c32fc3e0fe3edbb0c5d1497fedbb32af7
howbin <howbin13191201638@outlook.com>
2019-11-14 08:30:30 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Hello World!");
+
+			int j = 0;
+
+			for (int i=0; i<10; i++)
+			{
+
+				Console.WriteLine("wawawawaw\n");
+				j = i * i;
+			}
+
+		}
+	}
+}
